4defb33591df12ef742a6f63104f0b9ff3e025a0
Charles <amiguero809@gmail.com>
2018-07-18 20:00:25 +0200

    Add new AlbumList component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -9,10 +10,16 @@ class AlbumList extends Component {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => this.setState({ albums: response.data }));
     }
+    renderAlbulms() {
+         return this.state.albums.map(album => 
+         <AlbumDetail key={album.title} album={album} />
+        );
+    }
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Albulm List!!!!</Text>
+               {this.renderAlbulms()}
             </View>
         );
     }
