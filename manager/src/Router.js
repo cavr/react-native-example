7aa69af381f88cbd2fa1dea0ddebfecc35f35153
Charles <carlos.aaron.villanueva@gmail.com>
2018-10-15 17:43:14 +0200

    Add Complete Redux Form

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,7 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
-
+import EmployeeEdit from './components/EmployeeEdit';
 const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 65 }} >
@@ -24,6 +24,11 @@ const RouterComponent = () => {
                         key="employeeCreate"
                         component={EmployeeCreate}
                         title="Create Employee"
+                    />
+                    <Scene
+                        key="employeeEdit"
+                        component={EmployeeEdit}
+                        title="Edit Employee"
                     />
                 </Scene>
             </Scene>
--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,20 +1,47 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
-render(){
-    return(
-        <View>
-            <Text>Employee List</Text>
-            <Text>Employee List</Text>
-            <Text>Employee List</Text>
-            <Text>Employee List</Text>
-            <Text>Employee List</Text>
-            <Text>Employee List</Text>
-        </View>
-    );
-}
+    componentWillMount() {
+        this.props.employeesFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+        this.dataSource = ds.cloneWithRows(employees);
+    }
+    renderRow(employee) {
+        return <ListItem employee={employee} />
+    }
+    render() {
+
+        return (
+            <ListView
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
+    }
+};
+
+const mapStateToProps = state => {
+    console.log(state);
+    const employees = _.map(state.employees,
+        (val, uid) => { return { ...val, uid } });
+    return { employees };
 };
 
 
-export default EmployeeList;
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
